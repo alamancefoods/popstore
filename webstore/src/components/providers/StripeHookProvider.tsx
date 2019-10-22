5ee5e0963d76fe3e6e565b6ccf32defa29498cbf
MTempleman <huntertempleman@gmail.com>
2019-10-22 17:57:34 -0600

    Gonna go ahead and commit these changes before I break too many things.
    Attempting to cook up a Stripe hook using a github reference. If
    things work, the logic will be a lot more readable. (I find hooks more
    readable)

---
+++ webstore/src/components/providers/StripeHookProvider.tsx
@@ -0,0 +1,13 @@
+// Special Thanks to AndreRailean for hook hack!
+import React, { useContext, createContext } from 'react';
+import { injectStripe } from 'react-stripe-elements';
+
+const Context = createContext();
+
+export const useStripe = () => useContext(Context);
+
+const HookProvider = ({ children, stripe }) => {
+  return <Context.Provider value={stripe}>{children}</Context.Provider>;
+};
+
+export default injectStripe(HookProvider);
